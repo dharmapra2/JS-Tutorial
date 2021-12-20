644410a24030fa2a968b378b0c0c0d90bd2198e8
dharmapra2 <dharmapra865@gmail.com>
2021-12-20 20:07:45 +0530

    commit

---
+++ Test/com height.js
@@ -0,0 +1,9 @@
+const mheight=1.69,mmass=78;
+const jheight=1.95,jmass=92;
+let bmiofmark=mmass/mheight**2;
+let bmiofjhon=jmass/jheight**2;
+let height=bmiofjhon<bmiofmark;
+console.log(bmiofjhon,bmiofmark,height);
+let name="Dharma";
+let dp= name+" score occoured "+bmiofjhon;
+console.log(dp);

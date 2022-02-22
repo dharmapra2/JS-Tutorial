0fed7eb0bb4a7e8535b6ca5620aca2016d0f620b
dharmapra2 <dharmapra865@gmail.com>
2022-02-22 19:27:50 +0530

    commit

--- Mapty/assets/script.js
+++ Mapty/assets/script.js
@@ -10,50 +10,70 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map,mapEvent;
+// let map,mapEvent;
 
-// navigator.geolocation is work as callback function
-if (navigator.geolocation) {
-    //to get current positions of device
-    navigator.geolocation.getCurrentPosition(function (pos) {
-        // console.log(pos);
+class App{
+    #map;
+    #mapEvent;
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit',this._newWorkOut.bind(this));
+        // change inputs on type(cycle/running) mode
+        inputType.addEventListener('change', function () {
+            inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+            inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+        });
+    }
+    _getPosition() {
+        if (navigator.geolocation) {
+        //to get current positions of device
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
+            alert('could not get your position');
+        });
+        }
+    }
+    _loadMap(pos) {
         const { latitude,longitude } = pos.coords;
-        // console.log(latitude, longitude);
-        // console.log(`https://www.google.co.in/maps/@${latitude},${longitude}`);
         const coords = [ latitude, longitude ];
-        map = L.map('map').setView(coords, 13);
-        // console.log(map);
+        this.#map = L.map('map').setView(coords, 13);
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        // here the below on() is derived from leaflet lib. It is a eventListener also work as callback function
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
 
         //handling click on map
-        map.on('click', function (mapE) {
-            mapEvent = mapE;
+        this.#map.on('click', function (mapE) {
+            this.#mapEvent = mapE;
             form.classList.remove("hidden");
             inputDistance.focus();
         }) 
-    }, function () {
-        alert(`could not get your position`);
     }
-    );
+    _showForm() { }
+    _toggleElevationField() { }
+    _newWorkOut() {
+        e.preventDefault();
+        
+            //clear input fields
+            inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+            // display marker
+            //console.log(mapEvent);
+            const { lat, lng } = mapEvent.latlng;
+            L.marker({ lat, lng }).addTo(this.#map)
+                .bindPopup(
+                    L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: "running-popup"
+                    })
+                )
+                .setPopupContent('workout')
+                .openPopup();
+    }
 }
-form.addEventListener('submit', function () {
-    // display marker
-       console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker({ lat, lng }).addTo(map)
-        .bindPopup(
-        L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className:"running-popup"
-        })
-        )
-        .setPopupContent('workout')
-        .openPopup();
-})
+
+const workoutApp = new App();
+// workoutApp._getPosition();
+
+
+

63654d1387806f8b50f18f2c3db347d99fb3d524
dharmapra2 <dharmapra865@gmail.com>
2022-03-01 22:16:08 +0530

    commit

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,6 +1,13 @@
-'use strict';
+"use strict";
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/name/india");
+request.send();
+// console.log(request.responseText);
+request.addEventListener("load", function (){
+  console.log(this.responseText);
+});

67afd60d998111354fbab28cfe48161e88cc2591
dharmapra2 <dharmapra865@gmail.com>
2022-03-01 22:53:35 +0530

    commit

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -9,5 +9,25 @@ request.open("GET", "https://restcountries.com/v3.1/name/india");
 request.send();
 // console.log(request.responseText);
 request.addEventListener("load", function (){
-  console.log(this.responseText);
+  // console.log(this.responseText);
+  // destroring object to array
+  const [data] =JSON.parse(this.responseText);
+  // const data =JSON.parse(this.responseText);
+  console.log(data);
+  console.log(data.currencies['name']);
+  const html = `
+    <article class="country">
+    <img class="country__img" src="${data.flags['png']}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name['common']}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
+        <p class="country__row"><span>💰</span>${data.currencies
+        [0]}</p>
+      </div>
+    </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
